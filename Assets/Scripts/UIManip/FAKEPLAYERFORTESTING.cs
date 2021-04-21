aa83c63d52cf9a00194f2490dfeeb95e8015dda8
Allie! <80607680+IceQueenAllie@users.noreply.github.com>
2021-04-21 18:58:20 -0400

    added testing object
    introduced a fake player game object strictly for the ability to test a player specific script of adjusting health values before an eventual game handler is implemented.

---
+++ Assets/Scripts/UIManip/FAKEPLAYERFORTESTING.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+using BreakInfinity;
+using Character;
+using HPBar;
+
+public class FAKEPLAYERFORTESTING : MonoBehaviour 
+{
+    public hpBarManager hpb;
+
+    characterHandler hptesting = new characterHandler();
+    BigDouble fakeplayerchealth;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        DontDestroyOnLoad(this.gameObject);
+     fakeplayerchealth = hptesting.derivedStats._maxHP;
+     hpb.maxHpSet(fakeplayerchealth);
+    }
+    
+// Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            if (fakeplayerchealth > 0)
+            fakeplayerchealth -= 20;
+        }
+
+        hpb.currHpSet(fakeplayerchealth);
+    }
+}
