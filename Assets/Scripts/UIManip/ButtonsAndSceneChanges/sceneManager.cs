036b5f854081dbad823a02c6d17b5e7e5652ed4c
Allie! <80607680+IceQueenAllie@users.noreply.github.com>
2021-04-21 18:54:23 -0400

    Scene changes
    Introduced a scene in which the combat element will take place. Updated previous scenes with a new function to access the new combat scene. Introduced a game object in order to streamline scene changes into one single script.

---
+++ Assets/Scripts/UIManip/ButtonsAndSceneChanges/sceneManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class sceneManager : MonoBehaviour
+{
+    void Start()
+    {
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+    public void Scene1()
+    {
+        SceneManager.LoadScene("InvScene");
+    }
+    public void Scene2()
+    {
+        SceneManager.LoadScene("StatScene");
+    }
+
+    public void Scene3()
+    {
+        SceneManager.LoadScene("CombatScene");
+    }
+}
