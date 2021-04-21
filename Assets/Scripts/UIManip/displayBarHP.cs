d00b119d6baeefb4a754809067b0ea30eb4119b5
Allie! <80607680+IceQueenAllie@users.noreply.github.com>
2021-04-21 18:56:52 -0400

    healthbar introduced
    added a script to manage healthbars, as well as added the script in order to connect the slider of an hp bar to a text label in order to display a current health value

---
+++ Assets/Scripts/UIManip/displayBarHP.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class displayBarHP : MonoBehaviour
+{
+
+    Text hpl;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        hpl = GetComponent<Text> ();
+    }
+
+    // Update is called once per frame
+    public void textsUpdate(float value)
+    {
+        hpl.text = "Current Health: " + value;
+    }
+}
---
+++ Assets/Scripts/UIManip/hpBarManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+using BreakInfinity;
+using Character;
+
+
+namespace HPBar
+{
+    public class hpBarManager : MonoBehaviour
+    {
+
+        //New Object Creation
+
+        public Slider slider;
+
+        public void maxHpSet(BigDouble health)
+        {
+            string maxV = health.ToString();
+            float mV = float.Parse(maxV);
+            slider.maxValue = mV;
+            slider.value = mV;
+
+        }
+
+        public void currHpSet(BigDouble hp)
+        {
+            string currV = hp.ToString();
+            float cV = float.Parse(currV);
+            slider.value = cV;
+        }
+    }
+}
