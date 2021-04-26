5cb07ccef887529a5922cbeacbbbd34598be6569
ForgottenAngel <angelkanadetachibana150@gmail.com>
2021-04-26 01:13:55 -0400

    Update combatHandler.cs
    Reworked the code to utilize the new healing bool to allow for the healing aspect of certain hybrid skills to function. In addition, added a namespace for combatHandler. (Combat)

--- Assets/Scripts/combatHandler.cs
+++ Assets/Scripts/combatHandler.cs
@@ -10,111 +10,140 @@ using EnemyReferenceTable;
 using Abilities;
 using AbilityEnumTables;
 
-public class combatHandler : MonoBehaviour
+namespace Combat
 {
-    private bool combatActive;
-    private abilitiesCollection abilities = new abilitiesCollection();
-    private enemyCollection enemies = new enemyCollection();
-    public enemyHandler enemy = new enemyHandler();
-    
-    public void spawnEnemy(int enemyID)
+    public class combatHandler
     {
-        enemy = enemies.enemyList[enemyID];
-    }    
+        private bool combatActive;
+        private abilitiesCollection abilities = new abilitiesCollection();
+        private enemyCollection enemies = new enemyCollection();
+        public enemyHandler enemy = new enemyHandler();
 
-    public void doAbility(characterHandler player, int abilityID, bool enemyAction)
-    {
-        BigDouble dmgVal = abilities.abilityList[abilityID].Invoke();
-        abilityScaling scaling = abilities.abilityInfoList[abilityID].scaling;
-        AbilityType type = abilities.abilityInfoList[abilityID].type;
-
-        if (enemyAction == false)
+        public combatHandler()
         {
-            // Apply stat scaling to base value.
-            dmgVal *= (player.derivedStats._totalStr * scaling.strScale) + (player.derivedStats._totalInt * scaling.intScale)
-                      + (player.derivedStats._totalCon * scaling.conScale) + (player.derivedStats._totalWil * scaling.wilScale)
-                      + (player.derivedStats._pAttack * scaling.pAtkScale) + (player.derivedStats._mAttack * scaling.mAtkScale);
+            abilities = new abilitiesCollection();
+            enemies = new enemyCollection();
+            enemy = new enemyHandler();
+        }
 
-            // Handle Buffs + Debuffs
-            if (type == AbilityType.Buff || type == AbilityType.Debuff)
+        public void spawnEnemy(int enemyID)
+        {
+            enemy = enemies.enemyList[enemyID];
+            combatActive = true;
+        }
+
+        public bool checkEnemyAlive()
+        {
+            if (enemy.enemyStats._currentHP <= 0)
             {
-                List<int> effects = new List<int>();
-                effects = abilities.abilEffectList[abilityID].Invoke();
-
-                if (type == AbilityType.Buff)
-                {
-                    for (int i = 0; i < effects.Count; ++i)
-                    {
-                        
-                    }
-                }
-                else if (type == AbilityType.Debuff)
-                {
-
-                }
-            }
-            
-            // Handle Healing or Damage
-            if (type == AbilityType.Healing)
-            {
-                if (player.derivedStats._currHP + dmgVal > player.derivedStats._maxHP)
-                {
-                    player.derivedStats._currHP = player.derivedStats._maxHP;
-                } else
-                {
-                    player.derivedStats._currHP += dmgVal;
-                }
+                combatActive = false;
+                return false;
             } else
             {
-                enemy.enemyStats._currentHP -= dmgVal;
+                return true;
             }
-        } else
+        }
+
+        public void doAbility(characterHandler player, int abilityID, bool enemyAction)
         {
-            // Apply stat scaling to base value.
-            dmgVal *= (enemy.enemyStats._pAttack * 4 * scaling.strScale) + (enemy.enemyStats._mAttack * 4 * scaling.intScale)
-                      + (enemy.enemyStats._pDefense * 4 * scaling.conScale) + (enemy.enemyStats._mDefense * 4 * scaling.wilScale)
-                      + (enemy.enemyStats._pAttack * scaling.pAtkScale) + (enemy.enemyStats._mAttack * scaling.mAtkScale);
+            BigDouble dmgVal = abilities.abilityList[abilityID].Invoke();
+            abilityScaling scaling = abilities.abilityInfoList[abilityID].scaling;
+            AbilityType type = abilities.abilityInfoList[abilityID].type;
+            bool healing = abilities.abilityInfoList[abilityID].isHeal;
 
-            // Handle Buffs + Debuffs
-            if (type == AbilityType.Buff || type == AbilityType.Debuff)
+            // Determine ability user.
+            if (enemyAction == false)
             {
-                List<int> effects = new List<int>();
-                effects = abilities.abilEffectList[abilityID].Invoke();
+                // Apply stat scaling to base value.
+                dmgVal *= (player.derivedStats._totalStr * scaling.strScale) + (player.derivedStats._totalInt * scaling.intScale)
+                          + (player.derivedStats._totalCon * scaling.conScale) + (player.derivedStats._totalWil * scaling.wilScale)
+                          + (player.derivedStats._pAttack * scaling.pAtkScale) + (player.derivedStats._mAttack * scaling.mAtkScale);
 
-                if (type == AbilityType.Buff)
+                // Handle Buffs + Debuffs
+                if (type == AbilityType.Buff || type == AbilityType.Debuff)
                 {
-                    for (int i = 0; i < effects.Count; ++i)
+                    List<int> effects = new List<int>();
+                    effects = abilities.abilEffectList[abilityID].Invoke();
+
+                    if (type == AbilityType.Buff)
+                    {
+                        for (int i = 0; i < effects.Count; ++i)
+                        {
+
+                        }
+                    }
+                    else if (type == AbilityType.Debuff)
                     {
 
                     }
                 }
-                else if (type == AbilityType.Debuff)
-                {
 
-                }
-            }
-
-            // Handle Healing or Damage
-            if (type == AbilityType.Healing)
-            {
-                if (enemy.enemyStats._currentHP + dmgVal > enemy.enemyStats._maxHP)
+                // Handle Healing or Damage
+                if (type == AbilityType.Healing || healing == true)
                 {
-                    enemy.enemyStats._currentHP = enemy.enemyStats._maxHP;
+                    if (player.derivedStats._currHP + dmgVal > player.derivedStats._maxHP)
+                    {
+                        player.derivedStats._currHP = player.derivedStats._maxHP;
+                    }
+                    else
+                    {
+                        player.derivedStats._currHP += dmgVal;
+                    }
                 }
                 else
                 {
-                    enemy.enemyStats._currentHP += dmgVal;
+                    enemy.enemyStats._currentHP -= dmgVal;
                 }
-            } else
+            }
+            else if (enemyAction == true && combatActive == true)
             {
-                player.derivedStats._currHP -= dmgVal;
+                // Apply stat scaling to base value.
+                dmgVal *= (enemy.enemyStats._pAttack * 4 * scaling.strScale) + (enemy.enemyStats._mAttack * 4 * scaling.intScale)
+                          + (enemy.enemyStats._pDefense * 4 * scaling.conScale) + (enemy.enemyStats._mDefense * 4 * scaling.wilScale)
+                          + (enemy.enemyStats._pAttack * scaling.pAtkScale) + (enemy.enemyStats._mAttack * scaling.mAtkScale);
+
+                // Handle Buffs + Debuffs
+                if (type == AbilityType.Buff || type == AbilityType.Debuff)
+                {
+                    List<int> effects = new List<int>();
+                    effects = abilities.abilEffectList[abilityID].Invoke();
+
+                    if (type == AbilityType.Buff)
+                    {
+                        for (int i = 0; i < effects.Count; ++i)
+                        {
+
+                        }
+                    }
+                    else if (type == AbilityType.Debuff)
+                    {
+
+                    }
+                }
+
+                // Handle Healing or Damage
+                if (type == AbilityType.Healing)
+                {
+                    if (enemy.enemyStats._currentHP + dmgVal > enemy.enemyStats._maxHP)
+                    {
+                        enemy.enemyStats._currentHP = enemy.enemyStats._maxHP;
+                    }
+                    else
+                    {
+                        enemy.enemyStats._currentHP += dmgVal;
+                    }
+                }
+                else
+                {
+                    player.derivedStats._currHP -= dmgVal;
+                }
             }
         }
-    }
 
-    public bool _combatActive
-    {
-        get { return combatActive; }
-        set { combatActive = value; }
+        public bool _combatActive
+        {
+            get { return combatActive; }
+            set { combatActive = value; }
+        }
     }
 }
