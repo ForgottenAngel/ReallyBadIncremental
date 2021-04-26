7d0689c78e85e247bcd701191cc278531bac8532
ForgottenAngel <angelkanadetachibana150@gmail.com>
2021-04-26 01:25:04 -0400

    Update FAKEPLAYERFORTESTING.cs
    Added in test functions for simulating basic combat to help ensure functionality of preexisting code. In addition, changed Start to Awake to line up with other code.

--- Assets/Scripts/UIManip/FAKEPLAYERFORTESTING.cs
+++ Assets/Scripts/UIManip/FAKEPLAYERFORTESTING.cs
@@ -5,6 +5,7 @@ using UnityEngine.UI;
 
 using BreakInfinity;
 using Character;
+using Combat;
 using HPBar;
 
 public class FAKEPLAYERFORTESTING : MonoBehaviour 
@@ -12,26 +13,35 @@ public class FAKEPLAYERFORTESTING : MonoBehaviour
     public hpBarManager hpb;
 
     characterHandler hptesting = new characterHandler();
-    BigDouble fakeplayerchealth;
-
+    combatHandler combat = new combatHandler();
 
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
-        DontDestroyOnLoad(this.gameObject);
-     fakeplayerchealth = hptesting.derivedStats._maxHP;
-     hpb.maxHpSet(fakeplayerchealth);
+        hpb.maxHpSet(hptesting.derivedStats._maxHP);
     }
     
-// Update is called once per frame
+    // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.Space))
+        // Theoretical Combat Loop
+        if (Input.GetKeyDown(KeyCode.S))
         {
-            if (fakeplayerchealth > 0)
-            fakeplayerchealth -= 20;
+            combat.spawnEnemy(0);
+        } else if (Input.GetKeyDown(KeyCode.A))
+        {
+            combat.doAbility(hptesting, combat.enemy.abilityList[0], true);
+        } else if (Input.GetKeyDown(KeyCode.H))
+        {
+            combat.doAbility(hptesting, hptesting.abilityList[1], false);
         }
 
-        hpb.currHpSet(fakeplayerchealth);
+        // Check for dead enemy
+        if (combat.checkEnemyAlive() == false)
+        {
+            hptesting.incrementEXP(combat.enemy._exp);
+        }
+
+        hpb.currHpSet(hptesting.derivedStats._currHP);
     }
 }
