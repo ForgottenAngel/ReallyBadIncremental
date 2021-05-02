25221271bd2eab9c8d225859ca33b74778b15726
ForgottenAngel <angelkanadetachibana150@gmail.com>
2021-05-01 23:18:52 -0400

    Updated Inventory System
    Created separate sub-inventories dependent on item type and added relevant functions to interact with it. Removed redundant classes.

--- Assets/Scripts/Items/inventoryHandler.cs
+++ Assets/Scripts/Items/inventoryHandler.cs
@@ -7,19 +7,110 @@ using Items;
 
 namespace Inventory
 {
-    public readonly struct Constants
-    {
-        public const int ITEMS_PER_PAGE = 25;
-        public const int MAX_PAGES = 20;
-    }
-
-    public class equipHandler
-    {
-        private List<equipmentItem> charEquips = new List<equipmentItem>();
-    }
 
     public class inventoryHandler
     {
-        private List<equipmentItem> equipInv = new List<equipmentItem>();
+        private const int MAX_ITEMS = 24;
+        private Dictionary<int, equipmentItem> equipInv;
+        private Dictionary<int, healItem> consInv;
+
+        public inventoryHandler()
+        {
+            equipInv = new Dictionary<int, equipmentItem>();
+            consInv = new Dictionary<int, healItem>();
+        }
+
+        // Inventory Grabber Functions
+        public equipmentItem GetEquipItem(int slot)
+        {
+            return equipInv[slot];
+        }
+
+        public healItem GetConsumableItem(int slot)
+        {
+            return consInv[slot];
+        }
+
+
+        // Inventory Adder Functions
+        public bool AddEquipItem(equipmentItem item, int slot, bool firstOpenSlot)
+        {
+            if (!firstOpenSlot)
+            {
+                if (!equipInv.ContainsKey(slot))
+                {
+                    equipInv.Add(slot, item);
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            } else
+            {
+                for (int i = 0; i < MAX_ITEMS; ++i)
+                {
+                    if (!equipInv.ContainsKey(i))
+                    {
+                        equipInv.Add(i, item);
+                        return true;
+                    }
+                }
+
+                return false;
+            }
+        }
+
+        public bool AddConsumableItem(healItem item, int slot, bool firstOpenSlot)
+        {
+            if (!firstOpenSlot)
+            {
+                if (!consInv.ContainsKey(slot))
+                {
+                    consInv.Add(slot, item);
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            } else
+            {
+                for (int i = 0; i<MAX_ITEMS; ++i)
+                {
+                    if (!consInv.ContainsKey(i))
+                    {
+                        consInv.Add(i, item);
+                        return true;
+                    }
+                }
+
+                return false;
+            }
+        }
+
+        // Inventory Mover Function
+        public void moveItem(ItemCategory cat, int initialSlot, int targetSlot)
+        {
+            switch (cat)
+            {
+                case ItemCategory.Equipment:
+                    equipmentItem eI = new equipmentItem(equipInv[targetSlot]);
+                    equipInv.Remove(targetSlot);
+                    equipInv.Add(targetSlot, equipInv[initialSlot]);
+                    equipInv.Remove(initialSlot);
+                    equipInv.Add(initialSlot, eI);
+                    break;
+                case ItemCategory.Consumable:
+                    healItem hI = new healItem(consInv[targetSlot]);
+                    consInv.Remove(targetSlot);
+                    consInv.Add(targetSlot, consInv[initialSlot]);
+                    consInv.Remove(initialSlot);
+                    consInv.Add(initialSlot, hI);
+                    break;
+                default:
+                    break;
+            }
+        }
     }
 }
