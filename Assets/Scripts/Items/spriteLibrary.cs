87dc4ad0b1e69b707eec8b6f45ff8de033571e91
Allie! <80607680+IceQueenAllie@users.noreply.github.com>
2021-05-02 20:29:08 -0400

    Added Sprites
    added various sprites to the game as well as a library script to load them

---
+++ Assets/Scripts/Items/spriteLibrary.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace sLibrary
+{
+    public class spriteLibrary : MonoBehaviour
+    {
+       public Dictionary<int, Sprite> sprites;
+
+        // Start is called before the first frame update
+        void Awake()
+        {
+            sprites = new Dictionary<int, Sprite>();
+            sprites.Add(1, Resources.Load<Sprite>("swood"));
+        }
+
+    }
+}
